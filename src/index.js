ee19dd020c48d1e0be0f27e7c045b693ef0f9dc5
MohamedEMahmoud <mohamed.romyo99@gmail.com>
2022-09-04 13:06:05 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+const dbConnection = require('./config/database.config');
 const app = require('./app');
 
 (async () => {
-	const connectDB = await mongoose.connect(`${process.env.MONGO_URI}`);
-	if (!connectDB) {
-		throw new Error('Could not connect to MongoDB');
-	}
-	console.log('Connecting to MongoDB');
+	
+	dbConnection();
 
 	const port = process.env.PORT || 8000;
 
