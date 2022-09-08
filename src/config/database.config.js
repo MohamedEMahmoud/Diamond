c04c6eec53523c11ea88cf8c75e05fe0e5b76d37
MohamedEMahmoud <mohamed.romyo99@gmail.com>
2022-09-08 11:28:21 +0200

    update database config file

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-console.log(`${process.env.MONGO_URI}`);
 module.exports = async () => {
 	const connectDB = await mongoose.connect(`${process.env.MONGO_URI}`);
 	if (!connectDB) {
